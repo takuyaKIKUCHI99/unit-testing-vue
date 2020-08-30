a080bfc0f03d1e9f7fa257f4af4638f2b2ce502a
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-08-30 11:48:17 +0900

    Testing Props & User Interaction

---
+++ tests/unit/RandomNumber.spec.js
@@ -0,0 +1,39 @@
+import { mount } from "@vue/test-utils";
+import RandomNumber from "@/components/RandomNumber";
+
+describe("RandomNumber", () => {
+  // Output test
+  test("By default, randomNumber data value should be 0", () => {
+    const wrapper = mount(RandomNumber);
+    expect(wrapper.html()).toContain("<span>0</span>");
+  });
+
+  // Input(User Interaction) and Output test
+  test("If button is clicked, randomNumber should be between 1 and 10", async () => {
+    const wrapper = mount(RandomNumber);
+    wrapper.find("button").trigger("click");
+
+    await wrapper.vm.$nextTick();
+    const randomNumber = parseInt(wrapper.find("span").element.textContent);
+
+    expect(randomNumber).toBeGreaterThanOrEqual(1);
+    expect(randomNumber).toBeLessThanOrEqual(10);
+  });
+
+  // Input(Props) and Output test
+  test("If button is clicked, randomNumber should be between 200 and 300", async () => {
+    const wrapper = mount(RandomNumber, {
+      propsData: {
+        min: 200,
+        max: 300
+      }
+    });
+    wrapper.find("button").trigger("click");
+
+    await wrapper.vm.$nextTick();
+    const randomNumber = parseInt(wrapper.find("span").element.textContent);
+
+    expect(randomNumber).toBeGreaterThanOrEqual(200);
+    expect(randomNumber).toBeLessThanOrEqual(300);
+  });
+});
