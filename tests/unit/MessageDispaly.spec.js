b7e16b23ef1fead22e04d84cc20754b089baadb9
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-09-01 09:26:46 +0900

    Testing API Calls

---
+++ tests/unit/MessageDispaly.spec.js
@@ -0,0 +1,40 @@
+import MessageDisplay from "@/components/MessageDisplay";
+import { mount } from "@vue/test-utils";
+import { getMessage } from "@/services/axios";
+import flushPromises from "flush-promises";
+
+jest.mock("@/services/axios");
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+describe("MessageDisplay", () => {
+  it("Calls getMessage and displays message", async () => {
+    // mock the API call
+    const mockMessage = "Hello from the db";
+    getMessage.mockResolvedValueOnce({ text: mockMessage }); // calling our mocked get request
+    const wrapper = mount(MessageDisplay);
+    // wait for promise to resolve
+    await flushPromises();
+    // check that call happened once
+    expect(getMessage).toHaveBeenCalledTimes(1);
+    // check that component displays message
+    const message = wrapper.find('[data-testid="message"]').element.textContent;
+    expect(message).toEqual(mockMessage);
+  });
+
+  it("Displays an error when getMessage call fails", async () => {
+    // mock the failed API call
+    const mockError = "Oops! Something went wrong.";
+    getMessage.mockRejectedValueOnce(mockError);
+    const wrapper = mount(MessageDisplay);
+    // wait for promise to resolve
+    await flushPromises();
+    // check that call happened once
+    expect(getMessage).toHaveBeenCalledTimes(1);
+    // check that component displays error
+    const message = wrapper.find('[data-testid="message-error"]').element
+      .textContent;
+    expect(message).toEqual(mockError);
+  });
+});
